eb00e1330ee537b94e1a89b63330092f80f770c7
Gloria Spallina <g.spallina@interface3.be>
2021-01-13 17:01:03 +0100

    First commit

---
+++ MaPremiereApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaPremiereApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "2.2 Une page About qui vous présente";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "2.3 Une page contact qui affiche une carte de la rÃ©gion oÃ¹ vous habitez";
+
+            return View();
+        }
+
+        public ActionResult Hobbies()
+        {
+            ViewBag.Message = "Une page Hobbies qui liste vos passions, Hobbies,...";
+
+            return View();
+        }
+    }
+}
---
+++ MaPremiereApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MaPremiereApp.MvcApplication" Language="C#" %>
